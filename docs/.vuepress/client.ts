4c3df29ee6685d4c32838eb64b8d221655831c88
watermoon <2300013064@stu.pku.edu.cn>
2026-07-24 19:14:43 +0800

    feat: Vercel Stats

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,5 +1,15 @@
+import { inject as injectAnalytics } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
+import { onMounted } from 'vue'
 import { defineClientConfig } from 'vuepress/client'
 import 'lxgw-wenkai-webfont/lxgwwenkai-regular.css'
 import './styles/index.css'
 
-export default defineClientConfig({})
+export default defineClientConfig({
+  setup() {
+    onMounted(() => {
+      injectAnalytics()
+      injectSpeedInsights()
+    })
+  },
+})
